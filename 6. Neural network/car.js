eb32f5509bdc3f1fc0a7da3b67f80bdd780ba9e1
unknown <matt.mcquain@gmail.com>
2022-05-24 21:49:58 -0700

    neural network created. AI drives like a dingus. Starting visuals.

--- 6. Neural network/car.js
+++ 6. Neural network/car.js
@@ -1,140 +1,141 @@
-class Car{
-    constructor(x,y,width,height,controlType,maxSpeed=3){
-        this.x=x;
-        this.y=y;
-        this.width=width;
-        this.height=height;
+class Car {
+    constructor(x, y, width, height, controlType, maxSpeed = 3) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
 
-        this.speed=0;
-        this.acceleration=0.2;
-        this.maxSpeed=maxSpeed;
-        this.friction=0.05;
-        this.angle=0;
-        this.damaged=false;
+        this.speed = 0;
+        this.acceleration = 0.2;
+        this.maxSpeed = maxSpeed;
+        this.friction = 0.05;
+        this.angle = 0;
+        this.damaged = false;
 
-        this.useBrain=controlType=="AI";
+        this.useBrain = controlType == "AI"; //If the control type is AI, then we want to use the brain.
 
-        if(controlType!="DUMMY"){
-            this.sensor=new Sensor(this);
-            this.brain=new NeuralNetwork(
-                [this.sensor.rayCount,6,4]
+        if (controlType != "DUMMY") {
+            this.sensor = new Sensor(this);
+            this.brain = new NeuralNetwork(
+                [this.sensor.rayCount, 6, 4] //array of neuron counts. The size of the layers. Can add as many layers as we want. Here we have two. One hidden layer, and the output layer which will have four neurons. One for forward, backward, left, & right. 
             );
         }
-        this.controls=new Controls(controlType);
+        this.controls = new Controls(controlType);
     }
 
-    update(roadBorders,traffic){
-        if(!this.damaged){
+    update(roadBorders, traffic) {
+        if (!this.damaged) {
             this.#move();
-            this.polygon=this.#createPolygon();
-            this.damaged=this.#assessDamage(roadBorders,traffic);
+            this.polygon = this.#createPolygon();
+            this.damaged = this.#assessDamage(roadBorders, traffic);
         }
-        if(this.sensor){
-            this.sensor.update(roadBorders,traffic);
-            const offsets=this.sensor.readings.map(
-                s=>s==null?0:1-s.offset
-            );
-            const outputs=NeuralNetwork.feedForward(offsets,this.brain);
+        if (this.sensor) {
+            this.sensor.update(roadBorders, traffic);
+            const offsets = this.sensor.readings.map( //take out the offsets from the sensor readings. Readings have an x, y, and the offset of where the reading was.
+                s => s == null ? 0 : 1 - s.offset //'s' is the sensor reading. If it's null, then the offset is 0. Meaning there is no reading here. If it's not null, then return 1 - the sensor offset.
+            ); //We do this because we want the neurons to receive low values if the object is far away, and higher values (close to 1) if the objet is close.
+            const outputs = NeuralNetwork.feedForward(offsets, this.brain);
 
-            if(this.useBrain){
-                this.controls.forward=outputs[0];
-                this.controls.left=outputs[1];
-                this.controls.right=outputs[2];
-                this.controls.reverse=outputs[3];
+
+            if (this.useBrain) { //If we're using the brain, then we want to use the outputs from the brain to control the car.
+                this.controls.forward = outputs[0];
+                this.controls.left = outputs[1];
+                this.controls.right = outputs[2];
+                this.controls.reverse = outputs[3];
             }
         }
     }
 
-    #assessDamage(roadBorders,traffic){
-        for(let i=0;i<roadBorders.length;i++){
-            if(polysIntersect(this.polygon,roadBorders[i])){
+    #assessDamage(roadBorders, traffic) {
+        for (let i = 0; i < roadBorders.length; i++) {
+            if (polysIntersect(this.polygon, roadBorders[i])) {
                 return true;
             }
         }
-        for(let i=0;i<traffic.length;i++){
-            if(polysIntersect(this.polygon,traffic[i].polygon)){
+        for (let i = 0; i < traffic.length; i++) {
+            if (polysIntersect(this.polygon, traffic[i].polygon)) {
                 return true;
             }
         }
         return false;
     }
 
-    #createPolygon(){
-        const points=[];
-        const rad=Math.hypot(this.width,this.height)/2;
-        const alpha=Math.atan2(this.width,this.height);
+    #createPolygon() {
+        const points = [];
+        const rad = Math.hypot(this.width, this.height) / 2;
+        const alpha = Math.atan2(this.width, this.height);
         points.push({
-            x:this.x-Math.sin(this.angle-alpha)*rad,
-            y:this.y-Math.cos(this.angle-alpha)*rad
+            x: this.x - Math.sin(this.angle - alpha) * rad,
+            y: this.y - Math.cos(this.angle - alpha) * rad
         });
         points.push({
-            x:this.x-Math.sin(this.angle+alpha)*rad,
-            y:this.y-Math.cos(this.angle+alpha)*rad
+            x: this.x - Math.sin(this.angle + alpha) * rad,
+            y: this.y - Math.cos(this.angle + alpha) * rad
         });
         points.push({
-            x:this.x-Math.sin(Math.PI+this.angle-alpha)*rad,
-            y:this.y-Math.cos(Math.PI+this.angle-alpha)*rad
+            x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
+            y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad
         });
         points.push({
-            x:this.x-Math.sin(Math.PI+this.angle+alpha)*rad,
-            y:this.y-Math.cos(Math.PI+this.angle+alpha)*rad
+            x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
+            y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad
         });
         return points;
     }
 
-    #move(){
-        if(this.controls.forward){
-            this.speed+=this.acceleration;
+    #move() {
+        if (this.controls.forward) {
+            this.speed += this.acceleration;
         }
-        if(this.controls.reverse){
-            this.speed-=this.acceleration;
-        }
-
-        if(this.speed>this.maxSpeed){
-            this.speed=this.maxSpeed;
-        }
-        if(this.speed<-this.maxSpeed/2){
-            this.speed=-this.maxSpeed/2;
+        if (this.controls.reverse) {
+            this.speed -= this.acceleration;
         }
 
-        if(this.speed>0){
-            this.speed-=this.friction;
+        if (this.speed > this.maxSpeed) {
+            this.speed = this.maxSpeed;
         }
-        if(this.speed<0){
-            this.speed+=this.friction;
-        }
-        if(Math.abs(this.speed)<this.friction){
-            this.speed=0;
+        if (this.speed < -this.maxSpeed / 2) {
+            this.speed = -this.maxSpeed / 2;
         }
 
-        if(this.speed!=0){
-            const flip=this.speed>0?1:-1;
-            if(this.controls.left){
-                this.angle+=0.03*flip;
+        if (this.speed > 0) {
+            this.speed -= this.friction;
+        }
+        if (this.speed < 0) {
+            this.speed += this.friction;
+        }
+        if (Math.abs(this.speed) < this.friction) {
+            this.speed = 0;
+        }
+
+        if (this.speed != 0) {
+            const flip = this.speed > 0 ? 1 : -1;
+            if (this.controls.left) {
+                this.angle += 0.03 * flip;
             }
-            if(this.controls.right){
-                this.angle-=0.03*flip;
+            if (this.controls.right) {
+                this.angle -= 0.03 * flip;
             }
         }
 
-        this.x-=Math.sin(this.angle)*this.speed;
-        this.y-=Math.cos(this.angle)*this.speed;
+        this.x -= Math.sin(this.angle) * this.speed;
+        this.y -= Math.cos(this.angle) * this.speed;
     }
 
-    draw(ctx,color){
-        if(this.damaged){
-            ctx.fillStyle="gray";
-        }else{
-            ctx.fillStyle=color;
+    draw(ctx, color) {
+        if (this.damaged) {
+            ctx.fillStyle = "gray";
+        } else {
+            ctx.fillStyle = color;
         }
         ctx.beginPath();
-        ctx.moveTo(this.polygon[0].x,this.polygon[0].y);
-        for(let i=1;i<this.polygon.length;i++){
-            ctx.lineTo(this.polygon[i].x,this.polygon[i].y);
+        ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+        for (let i = 1; i < this.polygon.length; i++) {
+            ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
         }
         ctx.fill();
 
-        if(this.sensor){
+        if (this.sensor) {
             this.sensor.draw(ctx);
         }
     }
--- 6. Neural network/network.js
+++ 6. Neural network/network.js
@@ -1,66 +1,74 @@
-class NeuralNetwork{
-    constructor(neuronCounts){
-        this.levels=[];
-        for(let i=0;i<neuronCounts.length-1;i++){
-            this.levels.push(new Level(
-                neuronCounts[i],neuronCounts[i+1]
+class NeuralNetwork {
+    constructor(neuronCounts) { //constructor gets an array of neuronCounts (number of neurons in each layer)
+        this.levels = []; //we make our neural network out of an array of levels
+        for (let i = 0; i < neuronCounts.length - 1; i++) { //for each level, we specify the input and output count
+            this.levels.push(new Level( //we make a new level with the neuron counts from the ith index, and the i+1 index.
+                neuronCounts[i], neuronCounts[i + 1]
             ));
         }
     }
 
-    static feedForward(givenInputs,network){
-        let outputs=Level.feedForward(
-            givenInputs,network.levels[0]);
-        for(let i=1;i<network.levels.length;i++){
-            outputs=Level.feedForward(
-                outputs,network.levels[i]);
+    static feedForward(givenInputs, network) { //givenInputs and a network
+        //We get the outputs by calling the feedForward method from the level with the given inputs and the networks first level.
+        let outputs = Level.feedForward(
+            givenInputs, network.levels[0]); //calls first level to produce its outputs.
+        for (let i = 1; i < network.levels.length; i++) { // loop through remaining levels
+            outputs = Level.feedForward( //update the outputs with the feedForward result from the level i.
+                outputs, network.levels[i]); //we are putting in the outputs from the previous level in to the new level as the input.
         }
-        return outputs;
+        return outputs; //final outputs will tell us if the car should go forward, left, right, or reverse.
     }
 }
 
-class Level{
-    constructor(inputCount,outputCount){
-        this.inputs=new Array(inputCount);
-        this.outputs=new Array(outputCount);
-        this.biases=new Array(outputCount);
+// Levels of the Network
+class Level {
+    constructor(inputCount, outputCount) { //level has a layer of input & output neurons
+        this.inputs = new Array(inputCount); //values we get from the car sensors.
+        this.outputs = new Array(outputCount);
+        this.biases = new Array(outputCount); //'bias' is the value above which the neuron fires
 
-        this.weights=[];
-        for(let i=0;i<inputCount;i++){
-            this.weights[i]=new Array(outputCount);
+        //we will connect every input to every output neuron. The connections have 'weights'. 0 would be 0% of the connection, 1 would be 100% of the connection
+        this.weights = [];
+        for (let i = 0; i < inputCount; i++) { //goes through all inputs
+            this.weights[i] = new Array(outputCount); //creates an array of weights. For each input node we have an output count number of connections.
         }
 
-        Level.#randomize(this);
+        Level.#randomize(this); //randomized brain to start with
     }
 
-    static #randomize(level){
-        for(let i=0;i<level.inputs.length;i++){
-            for(let j=0;j<level.outputs.length;j++){
-                level.weights[i][j]=Math.random()*2-1;
+    //we write a static method because we want to serialize the object, and methods dont serialize.
+    static #randomize(level) {
+        for (let i = 0; i < level.inputs.length; i++) { //given a level, go through all inputs
+            for (let j = 0; j < level.outputs.length; j++) { //for each input, also go through its outputs
+                level.weights[i][j] = Math.random() * 2 - 1; //for each input/output pair, give it a random weight. Between -1 and 1.
             }
         }
 
-        for(let i=0;i<level.biases.length;i++){
-            level.biases[i]=Math.random()*2-1;
+        //we use/need negative values for biases because if the car is close to the wall, the bias will be more negative on that side, so the car will know to turn away from the wall.
+        for (let i = 0; i < level.biases.length; i++) {
+            level.biases[i] = Math.random() * 2 - 1;
         }
     }
 
-    static feedForward(givenInputs,level){
-        for(let i=0;i<level.inputs.length;i++){
-            level.inputs[i]=givenInputs[i];
+    //Feed Forward Algorithm
+    static feedForward(givenInputs, level) { //given some inputs, and a level, we go through all of the level inputs and set them to the given inputs. Those are the values from the sensor. 
+        for (let i = 0; i < level.inputs.length; i++) {
+            level.inputs[i] = givenInputs[i];
         }
 
-        for(let i=0;i<level.outputs.length;i++){
-            let sum=0
-            for(let j=0;j<level.inputs.length;j++){
-                sum+=level.inputs[j]*level.weights[j][i];
+        //Get Outputs
+        //to get the outputs, we loop through every outputs and calculate the sum of the value of the inputs and the weights.
+        for (let i = 0; i < level.outputs.length; i++) {
+            let sum = 0
+            for (let j = 0; j < level.inputs.length; j++) {
+                sum += level.inputs[j] * level.weights[j][i];
             }
 
-            if(sum>level.biases[i]){
-                level.outputs[i]=1;
-            }else{
-                level.outputs[i]=0;
-            } 
+            if (sum > level.biases[i]) { //is the found sum greater than the bias of the output neuron? If so, set output neuron to 1 (turning it 'on'). Else the output will be set to zero.
+                level.outputs[i] = 1;
+            } else {
+                level.outputs[i] = 0;
+            }
         }
 
         return level.outputs;
